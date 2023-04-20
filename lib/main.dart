fe7f595f84e753f7c16345cf1accbfcc7efc5c45
HannahJ0 <114255118+HannahJ0@users.noreply.github.com>
2023-04-20 13:13:09 -0700

    UI started
    Built basic UI with home page and link to Food Page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,102 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
+  const MyApp({Key? key}):super(key:key);
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Home',
+      home: const HomePage(title: 'HomePage'),
     );
+  }}
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key, required this.title}) : super(key:key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    Widget foodSection = Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.amber),
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            color: Colors.amber,
+          ),
+          padding: const EdgeInsets.all(50),
+          child: TextButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return const FoodHome(title: 'FoodHome');
+              }
+              )
+              );
+            }, // on pressed
+            child: Text("Food"),
+          ),
+        )
+    );
+    Widget exerciseSection = Expanded(
+        child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Colors.lightGreenAccent),
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              color: Colors.lightGreenAccent,
+            ),
+            padding: const EdgeInsets.all(50),
+            child: (
+                const Text('Exercise', textAlign: TextAlign.center,))
+        ));
+    Widget sleepSection = Expanded(
+        child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Colors.blueAccent),
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              color: Colors.blueAccent,
+            ),
+            padding: const EdgeInsets.all(50),
+            child: (
+                const Text('Sleep', textAlign: TextAlign.center,))
+        ));
+    return Scaffold(//MaterialApp(
+      // title: 'Everyday Wellness',
+      // home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: <Widget>[IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle_rounded),)],
+      ),
+
+      //body: const Center(
+      //  child: Text('Hello World'),
+      //),
+      body: Column(
+          children: [
+            Expanded (child: Row(children:[foodSection])),
+            Expanded (child: Row(children: [exerciseSection])),
+            Expanded (child: Row(children: [sleepSection])),
+            //Expanded (child: ButtonBar(children: [foodSection]))
+          ]
+      ),
+
+    );
+    //);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class FoodHome extends StatelessWidget{
+  const FoodHome({Key? key, required this.title}): super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
+        actions: <Widget>[IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle_rounded),)],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('hi'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
